b54c67e3425e40ac0c0e83654f4f68e3741c86e1
eeesiteadine <benmemurolucam@gmail.com>
2016-03-10 01:27:25 +0200

    initial commit

---
+++ sonn.js
@@ -0,0 +1,20 @@
+$("img").each(function() {
+
+    var src = "/res/genel_kültür00" + get_random(413) + ".jpg";
+    $(this).attr("src", src);
+});
+
+function get_random(x) {
+    var num = Math.floor(Math.random() * x) + 1;
+    if (num > 10 && num < 100) {
+        return "0" + num;
+    } else if (num < 10) {
+        return "00" + num;
+    } else {
+        return num;
+    }
+}
+$("img").on("click", function() {
+    var src = "/res/genel_kültür00" + get_random(413) + ".jpg";
+    $(this).attr("src", src);
+})
